e6bdc4f9a3c0355f51fcf3121b97fa4ef372d4f1
longmd40395 <longmd40395@sacombank.com>
2024-06-14 14:53:34 +0700

    Init code

---
+++ src/main/java/com/example/jenkinsgetstarted/JenkinsGetStartedApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsgetstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGetStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsGetStartedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkinsgetstarted/JenkinsGetStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsgetstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsGetStartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
